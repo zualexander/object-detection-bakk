1bd27b6d6b277dd676aefa29d65dac3dccfed5a5
zualexander <alexanderzulechner@gmail.com>
2021-03-14 16:39:34 +0100

    extracted color and width of label and bounding box as global constant

--- bounding-box-drawer/main.py
+++ bounding-box-drawer/main.py
@@ -9,6 +9,12 @@ IMAGE_RESULTS_PATH = './../test-images/results/'
 PATH_TO_PREDICTION_RESULTS = pathlib.Path(IMAGE_RESULTS_PATH)
 PREDICTION_RESULT_PATHS = sorted(list(PATH_TO_PREDICTION_RESULTS.glob("*.json")))
 
+LABEL_COLOR = (255, 0, 0)
+RECT_COLOR = (255, 0, 0)
+RECT_BORDER_WIDTH = 5
+LABEL_FONT_WIDTH = 1
+MIN_CONFIDENCE_TO_PRINT = 0.75
+
 if __name__ == '__main__':
     for result_path in PREDICTION_RESULT_PATHS:
         model_name = FileHandler.get_file_from_path_wo_ext(result_path)
@@ -28,17 +34,14 @@ if __name__ == '__main__':
 
             for prediction_object in prediction["objects"]:
                 coords_transformer = Coords_Transformator(img_path, prediction_object["relative_coordinates"])
-                if float(prediction_object['confidence']) > 0.75:
+                if float(prediction_object['confidence']) > MIN_CONFIDENCE_TO_PRINT:
                     coords = coords_transformer.get_coords_in_pixels()
                     min_tupel = (int(coords['xmin']), int(coords['ymin']))
                     max_tupel = (int(coords['xmax']), int(coords['ymax']))
 
-                    cv_image = cv2.rectangle(cv_image, min_tupel, max_tupel, (0, 255, 0))
-                    cv_image = cv2.putText(cv_image, prediction_object["name"], min_tupel, cv2.FONT_HERSHEY_COMPLEX,
-                                           0.5,
-                                           (0, 0, 0), 1)
+                    cv_image = cv2.rectangle(cv_image, min_tupel, max_tupel, RECT_COLOR, RECT_BORDER_WIDTH)
+                    cv_image = cv2.putText(cv_image, prediction_object["name"], min_tupel, cv2.FONT_HERSHEY_COMPLEX,1, LABEL_COLOR, LABEL_FONT_WIDTH)
 
-            # cv2.imshow("Show", cv_image)
             cv2.imwrite(
                 FileHandler.add_suffix_and_sub_directory(img_path, model_name, '--bounding-boxes'),
                 cv_image
